fbd18e8b6bf39f65aa860248f315f8e065ddc5f4
Kyle Bonar <kylewb2011@gmail.com>
2016-12-02 14:59:28 -0600

    Improved AI. Improved ball control. Added marker for missed balls. Added directions.

--- pong/pong.js
+++ pong/pong.js
@@ -1,50 +1,54 @@
-var canvas = document.createElement('canvas');
-canvas.width = 640;
-canvas.height = 400;
-canvas.id = "game-canvas";
-document.body.appendChild(canvas);
+
 
 function ballObject(width, height, radius) {
     this.x = width / 2; //start in center x
     this.y = height / 2; //start in center y
 
-    this.xspeed = -5; //initial velocty x
-    this.yspeed = -1; //initial velocity y
+    this.xspeed = 5; //initial velocty x
+    this.yspeed = 1; //initial velocity y
 
     this.radius = radius; //radius
 
     this.update = function() {
+        this.xspeed *= 1.0005; //speed up at game continues
         this.x += this.xspeed; //update x
         this.y += this.yspeed; //update y
 
-        //hit top
-        if (this.y < (0 + this.radius)) {
+
+        if (this.y < (0 + this.radius)) { //hit top
             this.yspeed = -this.yspeed;
-        }
-
-        //hit bottom
-        if (this.y > (canvas.height - this.radius)) {
+        } else if (this.y > (canvas.height - this.radius)) {//hit bottom
             this.yspeed = -this.yspeed;
-        }
-
-        //hit player paddle
-        if (this.y >= p1.y && this.y <= (p1.y + p1.height) && (this.x - this.radius) <= (p1.x + p1.width)) {
+        } else if (this.y > p1.y && this.y < (p1.y + p1.height) && (this.x - this.radius) <= (p1.x + p1.width)) { //hit player paddle
             this.xspeed = -this.xspeed;
-        }
-
-        //hit ai paddle
-        if (this.y >= ai.y && this.y <= (ai.y + ai.height) && (this.x + this.radius) >= (ai.x)) {
+            ai.state = "mad"; //playing hitting ball makes ai mad
+        } else if (this.y >= ai.y && this.y <= (ai.y + ai.height) && (this.x + this.radius) >= (ai.x)) { //hit ai paddle
             this.xspeed = -this.xspeed;
-        }
+            ai.state = "happy"; //ai hitting ball makes ai happy
+        } else if ( (this.x) < (p1.x + p1.width) || (this.x) > ai.x) {  //if ball goes off screen
+            this.xspeed = 0; //hold ball still
+            this.yspeed = 0; //hold ball still
 
-        //if ball goes off screen
-        if (this.x < 0 || this.x > canvas.width) {
-            this.x = width / 2;
-            this.y = height / 2;
-            this.xspeed = Math.random() > 0.5 ? -5 : 5; //give either positive or negative -4 depending on random number
-            this.yspeed = Math.random() > 0.5 ? (-1) * Math.floor((Math.random() * 4) + 1) : Math.floor((Math.random() * 4) + 1);
-            p1.resetPos();
-            ai.resetPos();
+            gameContext.beginPath(); //new ball showing where crossed line
+            gameContext.fillStyle = "red";
+            gameContext.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+            gameContext.fill();
+
+            setTimeout(function() {
+              ball.x = width / 2; //reset to middle
+              ball.y = height / 2; //reset to middle
+              p1.resetPos();
+              ai.resetPos();
+              setTimeout(function() {
+                if(ball.xspeed == 0 && ball.yspeed == 0 ){
+                    ball.xspeed = Math.random() > 0.5 ? -5 : 5; //give either positive or negative x speed
+                    ball.yspeed = Math.random() > 0.5 ? (-1) * Math.floor((Math.random() * 4) + 1) : Math.floor((Math.random() * 4) + 1);
+                }
+              }, 1000);
+            },1000);
+        } else {
+         this.xspeed = this.xspeed;
+         this.yspeed = this.yspeed;
         }
 
     }
@@ -68,10 +72,33 @@ function computer(width, height) {
     this.height = 60;
     this.y = height / 2 - this.height / 2;
     this.x = width - this.width;
+    this.state = "mad"; //happy if just hit the ball, mad if opponent just hit the ball
 
     this.resetPos = function() {
         this.y = height / 2 - this.height / 2;
         this.x = width - this.width;
+        this.state = "mad"; //reset state too
+    }
+
+    this.update = function() {
+      if( this.state == "mad" && ball.xspeed != 0) { //check paddle state and see if ball is actually moving
+        var y_distance = ball.y - (this.y + this.height / 2); //distance between ball and center of paddle
+        if( y_distance > 30 ) {
+            this.y += 3;
+        } else if( y_distance > 20 ) {
+            this.y += 2; //move ai down
+        } else if( y_distance > 0){
+            this.y += 1; //move down
+        } else if( y_distance < -30) {
+            this.y -= 3;
+        } else if( y_distance < -20) {
+            this.y -= 2;
+        } else if( y_distance < 0) {
+            this.y -= 1;
+        }
+      } else {
+        this.y = this.y; //do nothing
+      }
     }
 }
 
@@ -87,21 +114,37 @@ var ball;
 var p1;
 var ai;
 
+var instructions = document.createElement('p');
+instructions.innerHTML = "Click the canvas to begin the game. Press the up and down arrows on your keyboard to move your paddle. Have fun!";
+instructions.style.textAlign = "center";
+document.body.appendChild(instructions);
+
+var canvas = document.createElement('canvas');
+canvas.width = 640;
+canvas.height = 400;
+canvas.style.display = "block"; //make block
+canvas.style.margin = "0 auto"; //center block on screen
+canvas.id = "game-canvas";
+document.body.appendChild(canvas);
+
+
+
 window.onload = function() {
     gameCanvas = document.getElementById("game-canvas");
     gameContext = gameCanvas.getContext('2d');
-
     ball = new ballObject(canvas.width, canvas.height, 5); //new ball object
     p1 = new player(canvas.height); //new player paddle object
     ai = new computer(canvas.width, canvas.height); //new computer paddle object
+    draw();
 
-    setInterval(draw, 1000 / 30); //update at 30 fps
+    gameCanvas.addEventListener("click", function() {
+        setInterval(draw, 1000 / 30); //update at 30 fps
+    });
 }
 
 function draw() {
 
-    ai.y = ball.y - (ai.height / 2);
-    ball.update();
+
 
     gameContext.fillStyle = "black"; //board
     gameContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
@@ -112,16 +155,12 @@ function draw() {
     gameContext.fillStyle = "white"; //ai paddle
     gameContext.fillRect(ai.x, ai.y, ai.width, ai.height);
 
-    gameContext.beginPath();
+    gameContext.beginPath(); //ball
     gameContext.fillStyle = "white";
     gameContext.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI, false);
     gameContext.fill();
 
-
-    // gameContext.fillStyle = "white"; //ball
-    // gameContext.fillRect(ball_x, ball_y, ball_width, ball_height);
-
-
-
+    ball.update();
+    ai.update();
 
 }
