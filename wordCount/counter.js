de6a8bbe54b4390c954b2491688d407e36351c5c
Kyle Bonar <kylewb2011@gmail.com>
2016-12-06 15:57:00 -0600

    Fixed sizing issue with Bubbles. Add scaling factor soon.

--- wordCount/counter.js
+++ wordCount/counter.js
@@ -1,6 +1,6 @@
 var canvasElement = document.createElement('canvas');
-canvasElement.width = 600;
-canvasElement.height = 600;
+canvasElement.width = 920;
+canvasElement.height = 900;
 canvasElement.id = "game-canvas";
 document.body.appendChild(canvasElement);
 
@@ -13,6 +13,9 @@ var word;
 document.getElementById("submit").addEventListener("click", function() {
 	canvas = document.getElementById("game-canvas");
 	canvasArea = canvas.getContext('2d');
+
+	textArray = {}; //set to new every time
+	textKeys = [];//set to new every time
 
 	textBlock = document.getElementById("word_blob").value;
 	draw(textBlock);
@@ -46,25 +49,33 @@ function draw(textBlock) {
 
 	//master scaling ratio applied to all bubbles to match appropriate size to word count
 	//biggest bubble will be 20% (can change later)
-	var ratio = (canvasElement.width * canvasElement.height * 0.2) / (2 * Math.PI ) / ; 
+	// var ratio = (canvasElement.width * canvasElement.height * 0.2) / (2 * Math.PI ) / ;
+
+	var scalingFactor;
+	if(textArray[textKeys[i]] > 6000) {
+
+	}
+	var scalingFactor = textArray[textKeys[i]] >
 	//only interested in top n most-used words
 	for(let i = 0; i < 50; i++) {
 
-		var ball_r = ratio * textArray[textKey[i]]; //biggest 
-		var ball_x;
-		var ball_y;
-		
+		var surfaceArea = textArray[textKeys[i]]; //want total surface area to be the count value of the word
+		// var ball_r = ratio * textArray[textKey[i]]; //biggest
+		var ball_r = surfaceArea / (20 * Math.PI); //formula to get radius from surface area
+		var ball_x = ( Math.random()*canvas.width ) + 1;
+		var ball_y = ( Math.random()*canvas.height ) + 1
+
 		//make bubble
 		canvasArea.beginPath();
 		canvasArea.fillStyle = "white";
-		canvasArea.arc(ball_x, ball_y, ball_radius, 0, 2 * Math.PI, false );
+		canvasArea.arc(ball_x, ball_y, ball_r, 0, 2 * Math.PI, false );
 		canvasArea.fill();
 
 		//place word inside bubble
-		canvasArea.font = "30px Arial";
-		canvasArea.fillText(textArray[word], ball_x, ball_y);
+		// canvasArea.font = "30px Arial";
+		// canvasArea.fillText(textArray[word], ball_x, ball_y);
 
 	}
 
-	
+
 }
