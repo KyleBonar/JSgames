51cbbd99ee2b06236ccd1da763c6116c72ec8204
Kyle Bonar <kylewb2011@gmail.com>
2016-12-13 15:41:29 -0600

    Fixed X-Y positioning. Added user input processing. Changed color scheme.

--- phyllotaxis/phyllotaxis.js
+++ phyllotaxis/phyllotaxis.js
@@ -8,16 +8,24 @@ canvasArea = canvas.getContext('2d');
 canvasArea.fillStyle = "rgb(0,0,0)"; //board
 canvasArea.fillRect(0,0,canvas.width,canvas.height);
 
-var phi; //angle
+var phi; //angle get from user
 var r; //radius
-var n; //ordering number (?)
+var n; //size of flower
+var nMax; //get from user
 var c; //scaling factor
 var flower;
 var flowers = [];
+var deg2Rad;
 
-canvas.addEventListener("click", function() {
-  var mouseX = event.clientX;
-  var mouseY = event.clientY;
+
+
+canvas.addEventListener("click", function(e) {
+  var mouseX = e.offsetX;
+  var mouseY = e.offsetY;
+
+  //get from user
+  critAngle  = document.getElementById("angle").value;
+  nMax       = document.getElementById("size").value;
 
   c = 10;
   var flower = {
@@ -26,44 +34,44 @@ canvas.addEventListener("click", function() {
     x: 0,
     y: 0,
     phi: 0,
-    n: 0
+    n: 0,
+    red: Math.floor(Math.random()*255 + 1),
+    green: Math.floor(Math.random()*255 + 1),
+    blue: Math.floor(Math.random()*255 + 1),
+    criticalAngle: critAngle,
+    maxSize: nMax
   }
 
-  console.log(mouseX+" "+event.screenX);
+  // console.log(mouseX+" "+mouseY+" "+ e.offsetX+" "+e.offsetY);
   flowers.push(flower);
-  //setInterval(drawFlower, 1000/20); //refresh 10 fps
-
+  if(flowers.length==1) {
+    setInterval(drawFlower, 1000/25); //refresh 25 fps
+  }
 });
 
 
 
 function drawFlower() {
 
-
   for(let i = 0, length = flowers.length; i < length; i++) {
 
-    flowers[i].phi = flowers[i].n * 137.3; //formula from paper -- need to change to radians
+    if(flowers[i].n != flowers[i].maxSize) {
+    flowers[i].phi = flowers[i].n * flowers[i].criticalAngle; //formula from paper -- need to change to radians
+    deg2Rad = flowers[i].phi * Math.PI / 180;
     flowers[i].r   = c * Math.sqrt(flowers[i].n);
-    flowers[i].x   = flowers[i].r * Math.cos(flowers[i].phi * 180 / Math.PI) + canvas.width/2;
-    flowers[i].y   = flowers[i].r * Math.sin(flowers[i].phi * 180 / Math.PI) + flowers[i].startY;
+    flowers[i].x   = flowers[i].r * Math.cos( deg2Rad ) + flowers[i].startX;
+    flowers[i].y   = flowers[i].r * Math.sin( deg2Rad ) + flowers[i].startY;
     flowers[i].n   = flowers[i].n + 1;
 
-    // if(flowers[i].n === 50){ //stop drawing
-    //   flowers[i].phi = 0;
-    //   flowers[i].r   = 0;
-    //   flowers[i].x   = 0;
-    //   flowers[i].y   = 0;
-    //   flowers[i].n   = 50;
-    // } else {
-      canvasArea.beginPath(); //ball
-      canvasArea.fillStyle = "rgb("+ flowers[i].r % 255 +",255,255)";//change color based on radius
-      canvasArea.arc(flowers[i].x, flowers[i].y, 7, 0, 2 * Math.PI, false);
-      canvasArea.fill();
-    // }
 
-
+    // canvasArea.fillStyle = "rgb(255,"+ 155 + (flowers[i].n % 100)+",  0)";
+    // canvasArea.fillStyle = "rgb("+ flowers[i].r  % 255 +","+ flowers[i].r  % 255 +","+ flowers[i].r  % 255 +")";//change color based on radius
+    canvasArea.fillStyle = "rgb(" + (flowers[i].red + flowers[i].n) % 255 + "," + (flowers[i].green + flowers[i].n) % 255 + ","+ (flowers[i].blue + flowers[i].n)%255 +")";
+    canvasArea.beginPath(); //ball
+    canvasArea.arc(flowers[i].x, flowers[i].y, 7, 0, 2 * Math.PI, false);
+    canvasArea.closePath();
+    canvasArea.fill();
+    }
   }
-
-
 
 }
