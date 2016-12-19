b463693bdf6f37cf5ae977693bab3e3db40aa185
Kyle Bonar <kylewb2011@gmail.com>
2016-12-19 16:00:36 -0600

    Need to sort out user input to work along tree.

--- fractalTrees/fractal.js
+++ fractalTrees/fractal.js
@@ -1,6 +1,6 @@
 var canvasElement = document.createElement('canvas');
 canvasElement.width = 600;
-canvasElement.height = 600;
+canvasElement.height = 700;
 canvasElement.id = "game-canvas";
 document.getElementById("contentHolder").appendChild(canvasElement);
 canvas = document.getElementById("game-canvas");
@@ -11,16 +11,24 @@ canvasArea.fillRect(0,0,canvas.width,canvas.height);
 var begin = { x: canvas.width / 2, y: canvas.height};
 var end = { x: canvas.width/2, y: canvas.height - 100};
 var angle = 0;
-var branch;
 var tree = []; //store all branches
 var deg2Rad = Math.PI / 180.0;
+var tree_size = document.getElementById("tree_size");
+var depth = document.getElementById("branch_depth");
+var right_angle = document.getElementById("right_angle");
+var left_angle = document.getElementById("left_angle");
 
-function Branch(begin_x, begin_y, end_x, end_y, angle, width, depth) {
-  this.begin = { x: begin_x, y: begin_y }; //start x and y
-  this.end   = { x: end_x, y: end_y }; //end x and y
+function Branch(begin_x, begin_y, angle, width, depth) {
   this.angle = angle; //in deg
   this.lw    = width - 1; //line width
   this.depth = depth - 1;
+  this.drawn = false; //only want to draw branch if not already drawn
+  this.hasChildren = false; //does branch have children?
+
+
+  this.begin = { x: begin_x, y: begin_y }; //start x and y
+  this.end   = { x: this.begin.x + 5 * this.depth * Math.cos(this.angle * deg2Rad), y: this.begin.y + 5 * this.depth * Math.sin(this.angle * deg2Rad) }; //end x and y
+
 
   this.show = function() {
     canvasArea.beginPath();
@@ -34,40 +42,56 @@ function Branch(begin_x, begin_y, end_x, end_y, angle, width, depth) {
 
   //every branch should create two branches
   this.spawnChildLeft = function() {
-    var theta = this.angle - 20;
-    var x2 = this.end.x + 15 * this.depth * Math.cos(theta * deg2Rad);
-    var y2 = this.end.y + 15 * this.depth * Math.sin(theta * deg2Rad);
-
-    var left = new Branch(this.end.x, this.end.y, x2, y2, theta, this.lw, this.depth);
+    var theta = this.angle - left_angle.value;
+    var left = new Branch(this.end.x, this.end.y, theta, this.lw, this.depth);
     return left;
   }
 
   this.spawnChildRight = function() {
-    var phi = this.angle + 20;
-    var x2 = this.end.x + 15 * this.depth * Math.cos(phi * deg2Rad);
-    var y2 = this.end.y + 15 * this.depth * Math.sin(phi * deg2Rad);
-
-    var right = new Branch(this.end.x, this.end.y, x2, y2, phi, this.lw, this.depth);
+    var phi = this.angle + right_angle.value;
+    var right = new Branch(this.end.x, this.end.y, phi, this.lw, this.depth);
     return right;
   }
 
 }
 
-branch = new Branch(begin.x, begin.y, end.x, end.y, -90, 8, 5);
-tree.push(branch); //going to iterate over all branches
+tree.push(new Branch(begin.x, begin.y, -90, depth.value, depth.value)); //going to iterate over all branches
 displayTree();
 
+document.getElementById("reset_tree").addEventListener("click", resetTree);
+depth.addEventListener("change", function(){
+  document.getElementById("branch_depth_val").innerHTML = this.value;
+});
+right_angle.addEventListener("change", function(){
+  document.getElementById("right_angle_val").innerHTML = this.value;
+});
 canvas.addEventListener("click", function(e) {
   for(let i = tree.length-1; i >= 0; i--) {
-    tree.push(tree[i].spawnChildLeft());
-    tree.push(tree[i].spawnChildRight());
+    if(!tree[i].hasChildren && tree[i].depth!=0) {
+      tree.push(tree[i].spawnChildLeft()); //spawn left and push into array
+      tree.push(tree[i].spawnChildRight()); //spawn right and push into array
+      tree[i].hasChildren = true; //now branch has children
+    }
   }
-    displayTree();
+  displayTree();
 });
 
 
+
+function resetTree() {
+  canvasArea.fillStyle = "rgb(0,0,0)"; //black background
+  canvasArea.fillRect(0,0,canvas.width,canvas.height); //redraw board
+
+  tree.length=0; //reset array
+  tree.push(new Branch(begin.x, begin.y, -90, depth.value, depth.value)); //going to iterate over all branches
+  displayTree(); //draw
+}
+
 function displayTree() {
   for(let i = 0, length = tree.length; i < length; i++) {
-    tree[i].show();
+    if(!tree[i].drawn) { //if already drawn then we don't want to draw again
+      tree[i].show();
+      tree[i].drawn = true;
+    }
   }
 }
