396d45f1b76ade9b3e9fdfda7b3f90e8b7b08719
Kyle Bonar <kylewb2011@gmail.com>
2016-12-20 21:38:20 -0600

    Updated text and included a bit of directions on how to use application.

--- fractalTrees/index.php
+++ fractalTrees/index.php
@@ -1,8 +1,10 @@
 <?php include 'header.html'; ?>
 
 <div id='contentHolder'>
-  <h2>Phyllotaxis Clicker</h2>
-  <p>Click anywhere in the canvas to create a phyllotaxis image! The algorithm used to create this application, as well as a ton of other good information, can be found in <a href='http://algorithmicbotany.org/papers/abop/abop-ch4.pdf'>this</a> PDF. </p>
+  <h2>Fractal Tree</h2>
+  <p>Fractal Trees can be a fun visualization to represent a design which never ends and repeats itself. In this simple example, every branch creates two branches, which in turn, will each create two more branches, so forth and so on. To use this application, all you need to do is click on the canvas below! Each click will cause the existing branches to create their respective branch children using the angles, and length, you provide with the sliders. You can adjust these values at any time and reset the entire tree whenever you want.</p>
+  <p>Note: An angle of 0 will make the either of the branches go straight relative to their parent branch. An angle of 90, on the right branch, relates to a branch that will grow perpendicular, to the right, relative to it's parent while 90 on th left branch relates to a branch that will grow perpendicular, to the left, relative to its parent.</p>
+  <p>If all else fails, play around with the switches and have fun!</p>
   
   <hr />
   <div id='canvasHolder' style='width:80%'>
