9fa3b38b18ed4740f91b8f977f7e69383dd86992
Kyle Bonar <kylewb2011@gmail.com>
2016-12-22 10:23:54 -0600

    Start of double pendulum application.

---
+++ doublePend/index.php
@@ -0,0 +1,15 @@
+<?php include 'header.html'; ?>
+
+<div id='contentHolder'>
+	<h2>Double Pendulum</h2>
+	<p>Text here</p>
+
+	<hr />
+
+	<div id='canvasHolder' style='width=80%'>
+	</div>
+</div>
+
+<script src='doublePend.js'></script>
+
+<?php include 'footer.html'; ?> 
