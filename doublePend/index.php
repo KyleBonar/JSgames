f0dc3b2bd18ce58c1a54d222b9f09c1deea4231f
Kyle Bonar <kylewb2011@gmail.com>
2016-12-29 17:35:29 -0600

    Included font style. Applied font style. Added indentions.

--- doublePend/index.php
+++ doublePend/index.php
@@ -8,16 +8,16 @@
 
 	<div id='canvasHolder'>
 	</div>
-	<div id='controlHolder'>
-  Ball 1 mass: <input id='ball1_mass' type='range' min='1' max='20' step='1' value='10'><span id='ball1_mass_val'></span><br />
-  Ball 2 mass: <input id='ball2_mass' type='range' min='1' max='20' step='1' value='10'><span id='ball2_mass_val'></span><br />
-  String 1 length: <input id='string1_length' type='range' min='50' max='150' step='10' value='120'><span id='string1_length_val'></span><br />
-	String 2 length: <input id='string2_length' type='range' min='50' max='150' step='10' value='120'><span id='string2_length_val'></span><br />
-	Gravity: <input id='gravity' type='range' min='5' max='15' step='1' value='10'><span id='gravity_val'></span>
+		<div id='controlHolder'>
+	  Ball 1 mass: <input id='ball1_mass' type='range' min='1' max='20' step='1' value='10'><span id='ball1_mass_val'></span><br />
+	  Ball 2 mass: <input id='ball2_mass' type='range' min='1' max='20' step='1' value='10'><span id='ball2_mass_val'></span><br />
+	  String 1 length: <input id='string1_length' type='range' min='50' max='150' step='10' value='120'><span id='string1_length_val'></span><br />
+		String 2 length: <input id='string2_length' type='range' min='50' max='150' step='10' value='120'><span id='string2_length_val'></span><br />
+		Gravity: <input id='gravity' type='range' min='5' max='15' step='1' value='10'><span id='gravity_val'></span>
 
-  <br>
-  <button id='reset_pend'>Reset Pendulum</button><br />
-  <button id='go_pend'>Go!</button>
+	  <br>
+	  <button id='reset_pend'>Reset Pendulum</button><br />
+	  <button id='go_pend'>Go!</button>
   </div>
 </div>
 
