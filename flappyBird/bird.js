98bf6033ba34597c3235d6c03892eaa68bb294f1
Kyle Bonar <kylewb2011@gmail.com>
2017-02-23 22:08:04 -0600

    Designed generic square bird. Added means to stop bird in place.

--- flappyBird/bird.js
+++ flappyBird/bird.js
@@ -7,38 +7,70 @@ function Bird() {
 	this.velocity = 0; //y-velocity only
 	this.lift = -15; //force up
 
+	this.isAlive = true;
 
 	//draw bird
 	this.show = function() {
-		fill(255);
-		rect(this.x, this.y, this.r*2, this.r*2, 5);
-		// ellipse(this.x, this.y, this.r*2, this.r*2);
+
+		//top half of body
+		fill('#e91640');
+		noStroke();
+		rect(this.x, this.y, this.r*2, this.r, 5, 5, 0, 0);
+
+		//bottom half of body
+		fill('#dc143c');
+		noStroke();
+		rect(this.x, this.y+this.r, this.r*2, this.r, 0, 0, 5, 5);
+
+		//tail
+		fill('#e91640');
+		noStroke();
+		triangle(this.x+(this.r/3), this.y+(this.r/2), this.x+(this.r/3), this.y+(this.r*4/3), this.x-(this.r*2/3), this.y+(this.r/2));
+		
+		//eye
+		stroke(0,0,0);
+		fill(255,255,255);
+		ellipse(this.x+(this.r*3/2), this.y+(this.r*5/8), this.r*3/4, this.r*3/4);	
+		
+		//top beak
+		noStroke();
+		fill(255, 204, 0);
+		triangle(this.x+(this.r*2), this.y+(this.r/2), this.x+(this.r*2), this.y+this.r, this.x+(this.r*8/3), this.y+(this.r/2));
+		
+		//bottom beak
+		triangle(this.x+(this.r*2), this.y+this.r, this.x+(this.r*2), this.y+(this.r*3/2), this.x+(this.r*8/3), this.y+(this.r*3/2));
+	
+		fill(255,255,255);
 	}
 
 	//bird flap wings
 	this.flap = function() {
-		this.velocity += this.lift;
+		if(this.isAlive) {
+			this.velocity += this.lift;
+		}
 	}
 
 	//make bird fall with gravity
 	this.update = function() {
+		if(this.isAlive) {
+			//gravity pulls bird down with air resistance
+			this.velocity += this.gravity;
+			this.velocity *=0.9;
+			this.y += this.velocity;
+			// this.y = mouseY;
 
-		//gravity pulls bird down with air resistance
-		this.velocity += this.gravity;
-		this.velocity *=0.9;
-		this.y += this.velocity;
+			//if bird hits bottom, stop
+			if (this.y + this.r*2 > height ) {
+				this.y = height - this.r*2;
+				this.velocity = 0; 
+			}
 
-		//if bird hits bottom, stop
-		if (this.y > height - this.r) {
-			this.y = height - this.r;
-			this.velocity = 0; 
-		}
+			//if bird hits top, stop
+			if (this.y < 0) {
+				this.y = this.r;
+				this.velocity = 0;
 
-		//if bird hits top, stop
-		if (this.y < this.r) {
-			this.y = this.r;
-			this.velocity = 0;
-
+			}
 		}
 	}
 }
