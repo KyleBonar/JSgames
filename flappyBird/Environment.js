888c2c21a80010e37f49bdc73774025cb130b29b
Kyle Bonar <kylewb2011@gmail.com>
2017-02-26 17:23:25 -0600

    Now has a state for game running and when player dies. Also controls more of game flow now.

--- flappyBird/Environment.js
+++ flappyBird/Environment.js
@@ -1,24 +1,68 @@
 function Environment() {
 	this.score = 0; //initialize to zero
-	this.playerAlive = true;
-	this.marginOfError = 2;
+	this.playerAlive = false; //player must start game first
+	this.marginOfError = 2; //this value helps the game to feel more fluid when checking for contact
+	this.playerDeath = false;
 
 	this.background = function() {
-		//score becomes more green w/ higher score
-		background('rgb('+ (255-this.score*3) + ',255,' + (255-this.score*3) + ')');
+		if(this.playerAlive && !this.playerDeath) {
+			//score becomes more green w/ higher score
+			background('rgb('+ (240-this.score*3)%256 + ',240,' + (240-this.score*3)%256 + ')');
+		} else if (this.playerDeath) {
+			//same background as when alive
+			background('rgb('+ (240-this.score*3)%256 + ',240,' + (240-this.score*3)%256 + ')');
+
+			//show right side
+			fill(51);
+			rect(width/2, 0, width/2, height);
+			textSize(32);
+			textAlign(LEFT);
+			if(this.score != 0) {
+				fill('green');
+				text("Score: " + this.score, width*9/16, height/6);
+			}
+			fill(255);	
+			text("Press spacebar to RESET the game!", width*9/16, height/4, width-width*9/16, height);
+		
+		} else {
+			//generic background
+			background('rgb(240,240,240)');
+
+			//circle around bird
+			noStroke();
+			fill('rgba(240, 230, 140, 0.8)');
+			ellipse(bird.x+bird.r, bird.y+bird.r, 100, 100);
+		
+			//text on right side
+			fill(51);
+			rect(width/2, 0, width/2, height);
+			textSize(32);
+			textAlign(LEFT);
+			if(this.score != 0) {
+				fill('green');
+				text("Score: " + this.score, width*9/16, height/6);
+			}
+			fill(255);
+			text("Press spacebar to START the game!", width*9/16, height/4, width-width*9/16, height);
+		}	
 	}
 
 	this.showScore = function() {
 		//background for score
 		fill('rgba(210,210,210, .5)');
 		stroke(0 ,0 ,0);
-		rect( width/2-20, 0, 60, 50);
+		rect( width/2-30, 0, 60, 40);
 
 		//score
 		textSize(32);
 		fill(255, 0, 0);
 		noStroke();
+		textAlign(CENTER);
 		text(this.score, width/2, 30);
-
 	}
+
+	this.reset = function() {
+		this.score = 0;
+	}
+
 }
