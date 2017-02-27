f1aff8f2bbfc2e501d80ec296b5d9d9b76076df0
Kyle Bonar <kylewb2011@gmail.com>
2017-02-26 18:13:41 -0600

    Moved methods to prototype since there isn't a need for every wall to have the same methods.

--- flappyBird/wall.js
+++ flappyBird/wall.js
@@ -7,36 +7,30 @@ function Wall() {
 	this.w = 50; //wall width
 	this.velocity = 2; //x velocity
 	this.isTouched = false; //wall hasn't been touched by default
+}
 
-	this.show = function() {
-		if(this.isTouched){
-			fill("red");
-		} else {
-			fill(51);
-		}
-		
-		//draw top wall
-		rect(this.x, 0, this.w, this.top);
-		//draw bottom wall
-		rect(this.x, this.bottom, this.w, height - this.bottom);
-	}
+Wall.prototype.show = function() {
 
-	this.update = function() {
-		this.x -= this.velocity;
-	}
+	fill(51);
+	
+	//draw top wall
+	rect(this.x, 0, this.w, this.top);
+	//draw bottom wall
+	rect(this.x, this.bottom, this.w, height - this.bottom);
+}
 
-	//check if wall and bird make contact
-	this.contact = function(bird, score) {
-		//check x values
-		if(bird.x+bird.r*2 - environment.marginOfError > this.x && bird.x < this.x + this.w) {
-			//check y values
-			if (bird.y + environment.marginOfError < this.top || bird.y+bird.r*2 - environment.marginOfError >  this.bottom) {		
-				this.isTouched = true; //will change wall to red
-				environment.playerAlive = false;
-				bird.isAlive = false;
-				noLoop();
+Wall.prototype.update = function() {
+	this.x -= this.velocity;
+}
 
-			} 
-		}
+//check if wall and bird make contact
+Wall.prototype.contact = function(bird, score) {
+	//check x values
+	if(bird.x+bird.r*2 - environment.marginOfError > this.x && bird.x < this.x + this.w) {
+		//check y values
+		if (bird.y + environment.marginOfError < this.top || bird.y+bird.r*2 - environment.marginOfError >  this.bottom) {		
+			environment.playerAlive = false;
+			environment.playerDeath = true;
+		} 
 	}
 }
